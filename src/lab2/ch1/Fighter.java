53dc9e143973049aee1216860e5cad2420aa5661
unknown <smahagney@gmail.com>
2021-11-08 20:50:50 +0200

    lab2 OO

---
+++ src/lab2/ch1/Fighter.java
@@ -0,0 +1,32 @@
+package lab2.ch1;
+
+public class Fighter {
+    private static int counter;
+    private String name;
+    private int health;
+    private int damagePerAttack;
+
+    public Fighter(String name, int health, int damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+        counter++;
+    }
+
+
+    public void attack(Fighter f){
+        f.health = f.health - damagePerAttack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public static int getNumberOfFighters(){
+        return counter;
+    }
+}
